0a7f1efec7f4481e50c16a7c932db758d3ebce17
chris.han <namilhan@gmail.com>
2024-02-15 08:07:31 +0900

    Add webconfig

--- src/main/kotlin/chris/webfluxr2dbc/config/WebConfig.kt
+++ src/main/kotlin/chris/webfluxr2dbc/config/WebConfig.kt
@@ -1,2 +1,15 @@
-class WebConfig {
+package chris.webfluxr2dbc.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class WebConfig: WebFluxConfigurer {
+  override fun addCorsMappings(registry: CorsRegistry) {
+    registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .maxAge(3600)
+  }
 }
